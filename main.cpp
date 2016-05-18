fa17350f1f841ea040bb8ac4676e1be272187e6e
Isaac Lemus <ilemus@umich.edu>
2016-05-18 19:56:27 -0400

    One successful build, continuing work

--- main.cpp
+++ main.cpp
@@ -2,12 +2,29 @@
 
 using namespace std;
 
+#define INFINITY INT_MAX
+
 struct node {
     node *parent;
-    int x, y; //current x, y coordinates
+    char board[3][3];
     int f; //current distance from epoch
     bool color; //true: red, false: blue
 };
+
+struct list {
+    list *parent;
+    list *child;
+    node *q;
+};
+
+list *open_list;
+list *closed_list;
+
+char solution[][] {
+    {1, 0, 1},
+    {0, 0, 0},
+    {2, 0, 2}
+}
 
 /*  R = 2, B = 1
       0 1 2 j
@@ -16,7 +33,7 @@ struct node {
     2 B - B |
     i_______|
 */
-void setBoard(char board[3]) {
+void setBoard(char board[][3]) {
     // Clear Board
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
@@ -31,14 +48,73 @@ void setBoard(char board[3]) {
     board[2][2] = 1;
 }
 
-node* solve(char board[3]) {
+node* getLowestNode() {
+    node *lowest = open_list->q;
+    list *temp;
+    temp = open_list;
+    while (temp->parent != 0) {
+        if (temp->q->f < lowest->f) {
+            lowest = q;
+        }
+        
+        temp = temp->parent;
+    }
+    return lowest;
+}
+
+node* solve(char board[][3]) {
     node *q;
+    list *temp;
+    bool solved = false;
+    bool turn = true;
+    
+    // Setup queue and initial node
+    q->f = INFINITY;
+    setBoard(q->board);
+    q->board[0][1] = 9;
+    open_list->q = q;
+    open_list->parent = 0;
+    open_list->child = 0;
+    closed_list->q = 0;
+    closed_list->parent = 0;
+    closed_list->child = 0;
+    
     q->parent = q;
-    q->x = 0;
-    q->y = 0;
+    q->board = board;
     q->f = 0;
     q->color = true;
     
+    temp->child = 0;
+    temp->parent = open_list;
+    temp->q = q;
+    
+    open_list->child = temp;
+    open_list->parent = open_list;
+    
+    // Main loop
+    while (open_list->parent != 0) {
+        node *p;
+        *p = getLowestNode();
+        popNode(p);
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (valid(p, i, j)) {
+                    node *c->parent = p;
+                    if (isSolution(p)) {
+                        q = p;
+                        solved = true;
+                        break;
+                    } else {
+                        p->f = p->parent->f + 1;
+                    }
+                }
+            }
+        }
+        if (solved) {
+            break;
+        }
+    }
+    return q;
 }
 
 int main() {
